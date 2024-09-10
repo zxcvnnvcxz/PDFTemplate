352b86b8dce195e4aec348e156a01555102038a1
EDWARD <edward.kang0301@gmail.com>
2024-09-10 20:28:20 +1000

    Cleaned up line indent code

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
-x1 = 10
-y1 = 21
-x2 = 200
-y2 = 21
+
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -18,15 +15,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
 
-    y1 = 21
-    y2 = 21
-
-    # Add Lines
-    while y1 < 265 and y2 < 265:
-        pdf.line(x1, y1, x2, y2)
-        y1 = y1 + 10
-        y2 = y2 + 10
-
+    # Add lines
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)
@@ -34,16 +25,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
 
-    for i in range(row["Pages"]):
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-        y1 = 21
-        y2 = 21
 
-        # Add Lines
-        while y1 < 265 and y2 < 265:
-            pdf.line(x1, y1, x2, y2)
-            y1 = y1 + 10
-            y2 = y2 + 10
+        # Add lines
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
         # Set the footer
         pdf.ln(277)
