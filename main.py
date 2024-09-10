04af9bfe8826e3cbc8bca5f2d5c04d73f0382acb
EDWARD <edward.kang0301@gmail.com>
2024-09-10 19:50:59 +1000

    Add more pages for each topic using nested for loops

--- main.py
+++ main.py
@@ -17,4 +17,7 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align='L', ln=1)
     pdf.line(x1, y1, x2, y2)
 
+    for i in range(row["Pages"]):
+        pdf.add_page()
+
 pdf.output("output.pdf")
